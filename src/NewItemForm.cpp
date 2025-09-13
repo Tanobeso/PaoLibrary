c0172ae72f1834d675faf4cfd1e250de6e1dba15
Giuliano <giuliano.banchieri@studenti.unipd.it>
2025-09-13 22:29:23 +0200

    Messo path relativo immagine nella creazione item invece di assouluto

--- src/NewItemForm.cpp
+++ src/NewItemForm.cpp
@@ -5,7 +5,9 @@ void NewItemForm::onBrowse() {
     QString fileName = QFileDialog::getOpenFileName(this, "Select image",
                                                     QString(), "Images (*.png *.jpg *.jpeg *.bmp)");
     if (!fileName.isEmpty()) {
-        imageEdit->setText(fileName);
+        QDir currentDir = QDir::current();    //trasformo in path relativo per evitare di aver problemi con trasferimento libreria fra dispositivi
+        QString relativePath = currentDir.relativeFilePath(fileName);
+        imageEdit->setText(relativePath);
         QPixmap pix(fileName);
         if (!pix.isNull()) {
             imagePreview->setPixmap(pix.scaled(imagePreview->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
